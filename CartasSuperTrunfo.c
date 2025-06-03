28bd032691e4c54e51ef2c1b00b4eb21c9b406ad
Leticia Roque Rodrigues <leticiarr61@yahoo.com>
2025-06-03 11:11:45 +0000

    Variaveis atribuidas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Desafio Super Trunfo - Cadastro de Cartas
+
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Desafio Super Trunfo - Países
+
+    char Pais1[20], Pais2[20];
+    char Codigo1[4], Codigo2[4];
+    char Cidade1[20], Cidade2[20];
+    int Populacao1, Populacao2;
+    float Area1, Area2;
+    float PIB1, PIB2;
+    int pontos1, pontos2;
+
+    /*
+    Estado = Nome do País
+    Código = Código da carta 
+    Código da carta = O país é dividido em oito estados (A a H), e cada estado com quatro cidades (1 a 4). A combinação forma o código da carta (ex: A01, B02).
+    Cidade = Nome da cidade
+    População = Número total de habitantes
+    Area = Area em km²
+    PIB = PIB
+    Pontos = Número de pontos turísticos
+    */
 
     return 0;
 }
